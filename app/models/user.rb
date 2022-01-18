08502d148d248221cb7c0c17860f48b14ed5ff54
takabo-to <hsp.1king@icloud.com>
2022-01-18 15:19:56 +0900

    rubocopを実行し指摘箇所を自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true 
+  validates :first_name_kana, presence: true
   validates :birthday, presence: true
 end
