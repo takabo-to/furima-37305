0607abd065e73fa91694bd9d9b4926631994c680
takabo-to <hsp.1king@icloud.com>
2022-01-18 10:51:52 +0900

    マイグレーションファイルの修正

--- db/schema.rb
+++ db/schema.rb
@@ -16,10 +16,10 @@ ActiveRecord::Schema.define(version: 2022_01_17_112830) do
     t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "first_name", null: false
     t.string "last_name", null: false
-    t.string "first_name_kana", null: false
+    t.string "first_name", null: false
     t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
     t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
