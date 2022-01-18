95ee20dad312a0bb80cc2c1d054dba84c4bec0e0
takabo-to <hsp.1king@icloud.com>
2022-01-18 10:52:40 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validate :nickname, presence: true
-  validate :first_name, presence: true
-  validate :last_name, presence: true
-  validate :first_name_kana, presence: true
-  validate :last_name_kana, presence: true 
-  validate :birthday, presence: true
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true 
+  validates :birthday, presence: true
 end
