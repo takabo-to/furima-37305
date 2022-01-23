15affec1c09aaf5c456451165ca8b91db76109f1
takabo-to <hsp.1king@icloud.com>
2022-01-23 23:55:13 +0900

    edit,updateアクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,24 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if @item.user_id == current_user.id
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.valid?
+      redirect_to item_path(item_params)
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @item = Item.find(params[:id])
   end
