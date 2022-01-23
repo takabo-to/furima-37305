1e18398e325977ae4f42112cb5b769af5787cefe
takabo-to <hsp.1king@icloud.com>
2022-01-23 15:17:59 +0900

    rubocopの実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,36 +7,35 @@ RSpec.describe Item, type: :model do
 
   describe '商品の出品登録' do
     context '出品登録ができる場合' do
-     it 'すべての入力項目が存在すれば登録ができる' do
-      expect(@item).to be_valid
-     end
-     it 'カテゴリーが「---」以外であれば登録できる' do
-      @item.category_id = 1
-      expect(@item).to be_valid
-     end
-     it '商品の状態が「---」以外であれば登録ができる' do
-      @item.item_status_id = 1
+      it 'すべての入力項目が存在すれば登録ができる' do
         expect(@item).to be_valid
-     end
-     it '配送料の負担が「---」以外であれば登録ができる' do
-      @item.shipping_cost_id = 1
-      expect(@item).to be_valid
-     end
-     it '発送元地域が「---」以外であれば登録ができる' do
-      @item.prefecture_id = 1
-      expect(@item).to be_valid
-     end
-     it '発送までの日数が「---」以外であれば登録ができる' do
-      @item.shipping_date_id = 1
-      expect(@item).to be_valid
-     end
-     it '価格が半角数字かつ300円〜9,999,999円であれば登録できる' do
-      @item.price = 300
-      expect(@item).to be_valid
-     end
-
+      end
+      it 'カテゴリーが「---」以外であれば登録できる' do
+        @item.category_id = 1
+        expect(@item).to be_valid
+      end
+      it '商品の状態が「---」以外であれば登録ができる' do
+        @item.item_status_id = 1
+        expect(@item).to be_valid
+      end
+      it '配送料の負担が「---」以外であれば登録ができる' do
+        @item.shipping_cost_id = 1
+        expect(@item).to be_valid
+      end
+      it '発送元地域が「---」以外であれば登録ができる' do
+        @item.prefecture_id = 1
+        expect(@item).to be_valid
+      end
+      it '発送までの日数が「---」以外であれば登録ができる' do
+        @item.shipping_date_id = 1
+        expect(@item).to be_valid
+      end
+      it '価格が半角数字かつ300円〜9,999,999円であれば登録できる' do
+        @item.price = 300
+        expect(@item).to be_valid
+      end
     end
-  
+
     context '出品登録ができない場合' do
       it 'ユーザー登録していないと出品できない' do
         @item.user_id = nil
@@ -104,11 +103,6 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
-
-
-
     end
-      
   end
-
 end
