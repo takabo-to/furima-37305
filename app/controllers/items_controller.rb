2ff656c4e57ddf59eac6cf4330e3c8112c2b53b7
takabo-to <hsp.1king@icloud.com>
2022-01-24 12:29:40 +0900

    リファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,16 +20,14 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    if @item.user_id == current_user.id
-    else
+    if @item.user_id != current_user.id
       redirect_to root_path
     end
   end
 
   def update
     @item = Item.find(params[:id])
-    @item.update(item_params)
-    if @item.valid?
+    if @item.update(item_params)
       redirect_to item_path(item_params)
     else
       render 'edit'
