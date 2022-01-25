309d1c70588a2be05e3cc55434b67914b1d27b39
takabo-to <hsp.1king@icloud.com>
2022-01-25 16:57:37 +0900

    マイグレーションのカラムを追加（入れ忘れ）

--- db/migrate/20220124184653_create_deliveries.rb
+++ db/migrate/20220124184653_create_deliveries.rb
@@ -5,6 +5,7 @@ class CreateDeliveries < ActiveRecord::Migration[6.0]
       t.string :postcode, null: false
       t.integer :prefecture_id, null: false
       t.string :city, null: false
+      t.string :block, null: false
       t.string :building
       t.string :phone_number, null: false
 
--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2022_01_24_184653) do
     t.string "postcode", null: false
     t.integer "prefecture_id", null: false
     t.string "city", null: false
+    t.string "block", null: false
     t.string "building"
     t.string "phone_number", null: false
     t.datetime "created_at", precision: 6, null: false
